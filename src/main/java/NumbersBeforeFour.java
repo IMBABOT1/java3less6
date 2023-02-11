e5090e7abbfe89ad2ff5278ac1f8030bf7d966dc
unknown <zlovred12@gmail.com>
2023-02-12 01:45:12 +0300

    lesson6

--- src/main/java/NumbersBeforeFour.java
+++ src/main/java/NumbersBeforeFour.java
@@ -6,19 +6,30 @@ public class NumbersBeforeFour {
 
     public int[] numbers(int[] arr) {
         List<Integer> list = new ArrayList<>();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] != 4) {
-                list.add(arr[i]);
-            } else if (arr[i] == 4) {
-                break;
+        int counter = 0;
+        int[] result;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) {
+                counter++;
             }
         }
-        int[] result = new int[list.size()];
+        if (counter < 1) {
+            throw new RuntimeException("Value not found");
+        } else {
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (arr[i] != 4) {
+                    list.add(arr[i]);
+                } else if (arr[i] == 4) {
+                    break;
+                }
+            }
+            result = new int[list.size()];
 
-        for (int i = list.size() - 1, j = 0; i >= 0; i--, j++) {
-            result[j] = list.get(i);
+            for (int i = list.size() - 1, j = 0; i >= 0; i--, j++) {
+                result[j] = list.get(i);
+            }
         }
-
 
         return result;
     }
